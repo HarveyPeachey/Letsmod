0240a41097d4e656098ace1d4c5e5b7ab3ff46b8
Theruler333 <harveypeachey@hotmail.co.uk>
2013-07-21 14:44:09 +0100

    Block Constructor

---
+++ lmm_common/theruler333/lmm/lib/BlockConstructor.java
@@ -0,0 +1,16 @@
+package theruler333.lmm.lib;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockConstructor extends Block
+{
+        
+    public BlockConstructor (int id, Material material)
+    {
+            super(id, Material.rock);
+    }
+    
+}
+
+
