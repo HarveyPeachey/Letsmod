dd70e59e62bc16ae406cbbab32f3d377c2e11142
Kazi2015 <aranbacall@hotmail.co.uk>
2013-07-23 17:48:40 +0100

    snake 23 07 13 17:48

--- lmm_common/theruler333/lmm/block/Cobblestone_black.java
+++ lmm_common/theruler333/lmm/block/Cobblestone_black.java
@@ -3,13 +3,14 @@ package theruler333.lmm.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 
 import theruler333.lmm.lib.BlockIDs;;
 
-public class Cobblestone_black extends Block
+public class Cobblestone_black extends Block {
 
-        public Cobblestone_black (int Reference.BlockIDs.Cobblestone_black,  Material material)
-        //public Cobblestone_black (int 501, Material material)
+        public Cobblestone_black (int Cobblestone_black, Material stone)
         {
-                super(Cobblestone_black, material);
+                super(Cobblestone_black, stone);
         }
+}   
